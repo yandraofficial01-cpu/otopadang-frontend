658e4e6b9680b3a2d2ca66df963dafe43e8ce85e
yandraofficial01-cpu <yandraofficial01@gmail.com>
2026-07-31 00:18:17 +0700

    Update route.js

--- src/app/api/login/route.js
+++ src/app/api/login/route.js
@@ -1,1 +1,13 @@
+import { NextResponse } from "next/server"
 
+export async function POST(request) {
+  const body = await request.json()
+
+  const res = await fetch('https://otopadang-api.up.railway.app/auth/login', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(body)
+  })
+  const data = await res.json()
+  return NextResponse.json(data, { status: res.status })
+}
