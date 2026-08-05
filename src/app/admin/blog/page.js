a12ce34b0b73be208b6587d63ba260afa960e88a
yandraofficial01-cpu <yandraofficial01@gmail.com>
2026-08-05 16:16:05 +0700

    Update page.js

--- src/app/admin/blog/page.js
+++ src/app/admin/blog/page.js
@@ -1,1 +1,9 @@
-
+'use client'
+export default function KelolaBlogPage() {
+  return (
+    <div className="p-10 bg-[#0B0B0F] min-h-screen text-white">
+      <h1 className="text-2xl font-bold text-yellow-400">Kelola Blog</h1>
+      <p className="mt-4">Halaman ini masih kosong</p>
+    </div>
+  )
+}
