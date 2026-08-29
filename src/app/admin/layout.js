9176dc075111c1fec7b18847f22ee6f3fd7a43b2
yandraofficial01-cpu <yandraofficial01@gmail.com>
2026-08-29 13:08:47 +0700

    Create layout.js

---
+++ src/app/admin/layout.js
@@ -0,0 +1,14 @@
+import AutoLogout from "@/components/AutoLogout";
+
+export const metadata = {
+  title: "Admin Panel - Otopadang",
+};
+
+export default function AdminLayout({ children }) {
+  return (
+    <div className="bg-[#0B0B0F] min-h-screen">
+      <AutoLogout />
+      {children}
+    </div>
+  )
+}
