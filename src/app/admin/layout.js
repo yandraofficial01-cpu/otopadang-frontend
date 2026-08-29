3bcc41559449c67fc8b57c048a2ded8810e8b425
yandraofficial01-cpu <yandraofficial01@gmail.com>
2026-08-29 13:39:59 +0700

    Update layout.js

--- src/app/admin/layout.js
+++ src/app/admin/layout.js
@@ -1,14 +1,15 @@
-import AutoLogout from "@/component/autologout";
+import AutoLogout from "../component/autologout";
 
 export const metadata = {
   title: "Admin Panel - Otopadang",
+  description: "Dashboard Admin Otopadang",
 };
 
 export default function AdminLayout({ children }) {
   return (
-    <div className="bg-[#0B0B0F] min-h-screen">
+    <div>
       <AutoLogout />
       {children}
     </div>
-  )
+  );
 }
