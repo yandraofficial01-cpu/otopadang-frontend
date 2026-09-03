1795eed7aea2e3f458b0a617bfbbb1b31027f974
yandraofficial01-cpu <yandraofficial01@gmail.com>
2026-09-03 05:14:33 +0000

    Update layout.js

--- src/app/dashboard/mobil/layout.js
+++ src/app/dashboard/mobil/layout.js
@@ -1,19 +1,25 @@
 import { cookies } from 'next/headers'
 import { redirect } from 'next/navigation'
-import AutoLogout from "../../component/autologout";
+import AutoLogout from "../../../component/autologout"; // FIX: naik 3x folder
 
 export default function MobilLayout({ children }) {
   const cookieStore = cookies()
   const adminToken = cookieStore.get('admin_token')
   const showroomToken = cookieStore.get('showroom_token')
 
-  // KALAU DUA2NYA GAK ADA, TENDANG KE LOGIN SHOWROOM
+  // 1. KALAU GAK LOGIN SAMA SEKALI
   if (!adminToken && !showroomToken) {
-    redirect('/login-showroom') // ganti sesuai path login lu
+    redirect('/login-showroom') 
   }
 
+  // 2. KALAU YANG LOGIN ADMIN, JANGAN KASIH MASUK DASHBOARD SHOWROOM
+  if (adminToken && !showroomToken) {
+    redirect('/admin/rumah') 
+  }
+
+  // 3. KALAU YANG LOGIN SHOWROOM, BOLEH MASUK
   return (
-    <div className="min-h-screen bg-white">
+    <div className="min-h-screen bg-[#0B0B0F] text-[#E5E5E5]"> {/* UDAH PAKE TEMA GOLD LU */}
       <AutoLogout />
       {children}
     </div>
