28ebb1aaafe7090434cf6e365d8719edb60f1bf4
yandraofficial01-cpu <yandraofficial01@gmail.com>
2026-09-03 04:47:54 +0000

    Update layout.js

--- src/app/dashboard/mobil/layout.js
+++ src/app/dashboard/mobil/layout.js
@@ -1,6 +1,17 @@
+import { cookies } from 'next/headers'
+import { redirect } from 'next/navigation'
 import AutoLogout from "../../component/autologout";
 
 export default function MobilLayout({ children }) {
+  const cookieStore = cookies()
+  const adminToken = cookieStore.get('admin_token')
+  const showroomToken = cookieStore.get('showroom_token')
+
+  // KALAU DUA2NYA GAK ADA, TENDANG KE LOGIN SHOWROOM
+  if (!adminToken && !showroomToken) {
+    redirect('/login-showroom') // ganti sesuai path login lu
+  }
+
   return (
     <div className="min-h-screen bg-white">
       <AutoLogout />
