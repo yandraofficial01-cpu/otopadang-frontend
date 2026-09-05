99946b9e40e4377a1277411435f550fe824fd953
yandraofficial01-cpu <yandraofficial01@gmail.com>
2026-09-05 03:01:28 +0000

    Update layout.js

--- src/app/dashboard/mobil/layout.js
+++ src/app/dashboard/mobil/layout.js
@@ -1,26 +1,23 @@
 import { cookies } from 'next/headers'
 import { redirect } from 'next/navigation'
-import AutoLogout from "../../../component/autologout"; // FIX: naik 3x folder
+// import AutoLogout from "../../../component/autologout"; // KOMEN DULU
 
 export default function MobilLayout({ children }) {
   const cookieStore = cookies()
   const adminToken = cookieStore.get('admin_token')
   const showroomToken = cookieStore.get('showroom_token')
 
-  // 1. KALAU GAK LOGIN SAMA SEKALI
   if (!adminToken && !showroomToken) {
     redirect('/login-showroom') 
   }
-
-  // 2. KALAU YANG LOGIN ADMIN, JANGAN KASIH MASUK DASHBOARD SHOWROOM
+  
   if (adminToken && !showroomToken) {
     redirect('/admin/rumah') 
   }
 
-  // 3. KALAU YANG LOGIN SHOWROOM, BOLEH MASUK
   return (
-    <div className="min-h-screen bg-[#0B0B0F] text-[#E5E5E5]"> {/* UDAH PAKE TEMA GOLD LU */}
-      <AutoLogout />
+    <div className="min-h-screen bg-[#0B0B0F] text-[#E5E5E5]">
+      {/* <AutoLogout /> */}  {/* KOMEN DULU */}
       {children}
     </div>
   );
