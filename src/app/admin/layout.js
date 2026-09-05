578722a3f53ceab5eff6d3ab638f0a3582773b80
yandraofficial01-cpu <yandraofficial01@gmail.com>
2026-09-05 03:07:30 +0000

    Update layout.js

--- src/app/admin/layout.js
+++ src/app/admin/layout.js
@@ -1,4 +1,8 @@
-import AutoLogout from "../component/autologout";
+import { cookies } from 'next/headers' // WAJIB
+import { redirect } from 'next/navigation' // WAJIB
+import dynamic from 'next/dynamic' // WAJIB BUAT AUTO LOGOUT
+
+const AutoLogout = dynamic(() => import('../component/autologout'), { ssr: false })
 
 export const metadata = {
   title: "Admin Panel - Otopadang",
@@ -6,9 +10,17 @@ export const metadata = {
 };
 
 export default function AdminLayout({ children }) {
+  const cookieStore = cookies()
+  const adminToken = cookieStore.get('admin_token')
+
+  // KALAU GAK ADA TOKEN ADMIN, TENDANG KE LOGIN ADMIN
+  if (!adminToken) {
+    redirect('/login-admin')
+  }
+
   return (
-    <div className="bg-[#0B0B0F] min-h-screen">
-      <AutoLogout /> {/* 1 jam baru logout */}
+    <div className="bg-[#0B0B0F] min-h-screen text-[#E5E5E5]">
+      <AutoLogout />
       {children}
     </div>
   );
