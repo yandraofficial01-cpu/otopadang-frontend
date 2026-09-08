22f8f30404e4e4ab9aa20971c1dea77693954aa4
yandraofficial01-cpu <yandraofficial01@gmail.com>
2026-09-08 17:08:28 +0000

    Update layout.js

--- src/app/admin/layout.js
+++ src/app/admin/layout.js
@@ -1,17 +1,11 @@
-import Autologoutwrapper from '../../component/autologoutwrapper'
-
 export const metadata = {
   title: "Admin Panel - Otopadang",
   description: "Dashboard Admin Otopadang",
 };
 
 export default function AdminLayout({ children }) {
-  // HAPUS SEMUA CEK COOKIE DI SINI
-  // Biarkan page.js yang handle redirect via /api/auth/me
-
   return (
     <div className="bg-[#0B0B0F] min-h-screen text-[#E5E5E5]">
-      <Autologoutwrapper />
       {children}
     </div>
   );
